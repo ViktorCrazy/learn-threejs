799b19b3badbcf0d1406234ed6773b79e30532ee
viktorschuster <schusterviktor7@gmail.com>
2024-12-31 10:36:23 +0100

    stop rotation

--- src/main.js
+++ src/main.js
@@ -25,8 +25,8 @@ camera.position.z = 5;
 // Create an animation loop
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  // cube.rotation.x += 0.01;
+  // cube.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
 
