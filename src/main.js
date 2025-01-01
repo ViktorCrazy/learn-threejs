7f0c3ca5d7c79c5bc143815e4fd3f83399bd2d4d
viktorschuster <schusterviktor7@gmail.com>
2025-01-01 12:14:33 +0100

    ligths demo controls

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper';
+import * as dat from 'dat.gui';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -79,6 +80,63 @@ rectLight.add(rectLightHelper);
 // LightProbe
 const lightProbe = new THREE.LightProbe();
 scene.add(lightProbe);
+
+// dat.GUI
+const gui = new dat.GUI();
+
+// Box controls
+const boxFolder = gui.addFolder('Box');
+boxFolder.add(box.position, 'x', -10, 10).name('Position X');
+boxFolder.add(box.position, 'y', 0, 10).name('Position Y');
+boxFolder.add(box.position, 'z', -10, 10).name('Position Z');
+boxFolder.addColor(boxMaterial, 'color').name('Color');
+boxFolder.open();
+
+// Ambient Light controls
+const ambientLightFolder = gui.addFolder('Ambient Light');
+ambientLightFolder.add(ambientLight, 'intensity', 0, 1).name('Intensity');
+ambientLightFolder.open();
+
+// Directional Light controls
+const directionalLightFolder = gui.addFolder('Directional Light');
+directionalLightFolder.add(directionalLight.position, 'x', -20, 20).name('Position X');
+directionalLightFolder.add(directionalLight.position, 'y', -20, 20).name('Position Y');
+directionalLightFolder.add(directionalLight.position, 'z', -20, 20).name('Position Z');
+directionalLightFolder.add(directionalLight, 'intensity', 0, 2).name('Intensity');
+directionalLightFolder.open();
+
+// Point Light controls
+const pointLightFolder = gui.addFolder('Point Light');
+pointLightFolder.add(pointLight.position, 'x', -20, 20).name('Position X');
+pointLightFolder.add(pointLight.position, 'y', -20, 20).name('Position Y');
+pointLightFolder.add(pointLight.position, 'z', -20, 20).name('Position Z');
+pointLightFolder.add(pointLight, 'intensity', 0, 2).name('Intensity');
+pointLightFolder.open();
+
+// Spot Light controls
+const spotLightFolder = gui.addFolder('Spot Light');
+spotLightFolder.add(spotLight.position, 'x', -20, 20).name('Position X');
+spotLightFolder.add(spotLight.position, 'y', -20, 20).name('Position Y');
+spotLightFolder.add(spotLight.position, 'z', -20, 20).name('Position Z');
+spotLightFolder.add(spotLight, 'intensity', 0, 2).name('Intensity');
+spotLightFolder.add(spotLight, 'angle', 0, Math.PI / 2).name('Angle');
+spotLightFolder.add(spotLight, 'penumbra', 0, 1).name('Penumbra');
+spotLightFolder.open();
+
+// Hemisphere Light controls
+const hemisphereLightFolder = gui.addFolder('Hemisphere Light');
+hemisphereLightFolder.add(hemisphereLight, 'intensity', 0, 1).name('Intensity');
+// hemisphereLightFolder.addColor(hemisphereLight, 'skyColor').name('Sky Color');
+hemisphereLightFolder.addColor(hemisphereLight, 'groundColor').name('Ground Color');
+hemisphereLightFolder.open();
+
+// Rect Area Light controls
+const rectLightFolder = gui.addFolder('Rect Area Light');
+rectLightFolder.add(rectLight.position, 'x', -20, 20).name('Position X');
+rectLightFolder.add(rectLight.position, 'y', -20, 20).name('Position Y');
+rectLightFolder.add(rectLight.position, 'z', -20, 20).name('Position Z');
+rectLightFolder.add(rectLight, 'intensity', 0, 10).name('Intensity');
+rectLightFolder.open();
 
 // Animation Loop
 function animate() {
